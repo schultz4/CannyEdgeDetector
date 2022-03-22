d7bc0d6a20e03da2670471d805bf2d2165dedb43
Catherine Anne McQueen <catmcqueen@i16n0.ocelote.hpc.arizona.edu>
2022-03-22 15:17:18 -0700

    fixed cannyEdgeDetector

--- canny/cannyEdgeDetector.cpp
+++ canny/cannyEdgeDetector.cpp
@@ -79,14 +79,16 @@ void read_png_file(char *filename) {
 
 
 void process_png_file() {
-  for(int y = 0; y < height; y++) {
-    png_bytep row = row_pointers[y];
-    for(int x = 0; x < width; x++) {
-      png_bytep px = &(row[x * 4]);
+  //for(int y = 0; y < height; y++) {
+    //png_bytep row = row_pointers[y];
+    //for(int x = 0; x < width; x++) {
+      //png_bytep px = &(row[x * 4]);
       // Do something awesome for each pixel here...
       //printf("%4d, %4d = RGBA(%3d, %3d, %3d, %3d)\n", x, y, px[0], px[1], px[2], px[3]);
-    }
-  }
+	printf("Height %4d Width %4d \n", height, width);	
+
+   // }
+  //}
 }
 
 int main(int argc, char *argv[]) {
