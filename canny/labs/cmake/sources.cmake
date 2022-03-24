7c277edc8314c8a8616472429a67a1cbdf0ba619
Chris Schultze <schultz4@email.arizona.edu>
2022-03-24 02:58:34 -0700

    Adding libwb as it exists from homeworks (see https://github.com/abduld/libwb for original git repo). Original git repo locked so stripped .git for this to clone-and-own with headless revs from Dr. Akoglu

---
+++ canny/labs/cmake/sources.cmake
@@ -0,0 +1,2 @@
+add_lab("CannyImage")
+add_lab_solution("CannyImage" ${PROJECT_SOURCE_DIR}/solution.cu)
--- canny/labs/sources.cmake
+++
@@ -1,2 +0,0 @@
-add_lab("CannyImage")
-add_lab_solution("CannyImage" ${CMAKE_CURRENT_LIST_DIR}/solution.cu)
