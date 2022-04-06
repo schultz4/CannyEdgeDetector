56497c368a0f1356c7f946cbe3fd09de5a595799
Chris Schultze <schultz4@email.arizona.edu>
2022-04-06 09:31:29 -0700

    Updating to build original executable

--- canny/labs/cmake/sources.cmake
+++ canny/labs/cmake/sources.cmake
@@ -1,8 +1,8 @@
 add_lab("CannyImage")
 #add_lab_solution("CannyImage" ${PROJECT_SOURCE_DIR}/solution.cu)
-set(sources ${PROJECT_SOURCE_DIR}/Otsus_Method_Sequential.cu ${PROJECT_SOURCE_DIR}/solution.cu)
-cuda_add_executable(CannyImage_Solution ${sources})
-target_link_libraries(CannyImage_Solution ${WBLIB} )
+#set(sources ${PROJECT_SOURCE_DIR}/Otsus_Method_Sequential.cu ${PROJECT_SOURCE_DIR}/solution.cu)
+#cuda_add_executable(CannyImage_Solution ${sources})
+#target_link_libraries(CannyImage_Solution ${WBLIB} )
 
 # Splitting out executables for my sanity
 
@@ -38,6 +38,7 @@ set( target_canny_serial CannyImage_Serial )
 set( canny_serial_libs
   ${target_filters}
   ${target_otsu}
+  ${WBLIB} 
   )
 
 set( canny_serial_sources
@@ -46,5 +47,22 @@ set( canny_serial_sources
   ) 
 
 add_executable( ${target_canny_serial} ${canny_serial_sources} )
-target_link_libraries( ${target_canny_serial} ${WBLIB} ${canny_serial_libs} ) 
+target_link_libraries( ${target_canny_serial} ${canny_serial_libs} ) 
 
+################################################################################
+# Build CUDA executables
+################################################################################
+set(target_canny_gpu CannyImage_Solution )
+
+set( canny_gpu_libs
+  ${target_filters}
+  ${target_otsu}
+  ${WBLIB} 
+  )
+
+set(sources_gpu 
+  ${PROJECT_SOURCE_DIR}/solution.cu
+  )
+
+cuda_add_executable(${target_canny_gpu} ${sources_gpu})
+target_link_libraries( ${target_canny_gpu} ${canny_gpu_libs} ) 
