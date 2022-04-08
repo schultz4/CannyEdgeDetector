94e9fa59c89cfd53b1c8602bdedafb360258b1fa
Chris Schultze <schultz4@email.arizona.edu>
2022-04-08 14:11:01 -0700

    Fixed image output. Implemented serial grayscale image conversion. Updated runscript. Uncovered memory issues.

--- canny/labs/filters.cu
+++ canny/labs/filters.cu
@@ -42,6 +42,19 @@ void populate_blur_filter(double outFilter[FILTERSIZE][FILTERSIZE])
 }
 
 
+void ColorToGrayscaleSerial(float *input, float *output,
+                    unsigned int y, unsigned int x) {
+  for (unsigned int ii = 0; ii < y; ii++) {
+    for (unsigned int jj = 0; jj < x; jj++) {
+      unsigned int idx = ii * x + jj;
+      float r          = input[3 * idx];     // red value for pixel
+      float g          = input[3 * idx + 1]; // green value for pixel
+      float b          = input[3 * idx + 2];
+      output[idx] = (float)(0.21f * r + 0.71f * g + 0.07f * b);
+    }
+  }
+}
+
 // convert the image to grayscale
 __global__ void ColorToGrayscale(float *inImg, float *outImg, int width, int height) {
    int idx, grayidx;
