527a3cf8ae3012e2602524f1b33393b34ffc23df
ThomasWHarp <Twharp345@gmail.com>
2022-04-18 23:20:55 -0400

    Latest Code
    adding native and starting shared

--- canny/labs/Edge_Connection.cu
+++ canny/labs/Edge_Connection.cu
@@ -157,3 +157,84 @@ __global__ void edge_connection_global(float *weak_img, float *edge_img, int wid
         }
     }
 }
+
+/*
+__global__ void edge_connection_shared(float *weak_img, float *edge_img, int width, int height) {
+
+    // Set Tile wiidth
+    int TILE_WIDTH = 16;
+
+    // Size of edge screach
+    int edge_size = 1;
+
+    // Set up thread ID
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    // Set up row and column with edge
+    int Col = tx + bx * (blockDim.x - 2 * edge_size);
+    int Row = ty + by * (blockDim.y - 2 * edge_size);
+
+    // Set bounds for edges of image
+    if ((Col < width + edge_size) && (Row < height + edge_size)) {
+
+        // Allocate shared memory
+        // Has an extra pixel on each side of the tile for neibouring image seach
+        __shared__ float chuck[TILE_WIDTH + (2 * edge_size)][TILE_WIDTH + (2 * edge_size)];
+
+        // Set row and column of image without extra boundary pixel
+        int rel_row = Row - edge_size;
+        int rel_col = Col - edge_size;
+
+        // Read image data into tile
+        // If pixel is outside image, set to zero
+        if ((rel_row < height) && (rel_col < width) && (rel_row >= 0) && (rel_col >=0)) {
+            edge_chunk[ty][tx] = edge_img[rel_row * width + rel_col];
+            weak_chunk[ty][tx] = weak_img[rel_row * width + rel_col];
+        } else {
+            edge_chunk[ty][tx] = 0;
+            weak_chunk[ty][tx] = 0;
+        }
+
+        __syncthreads();
+
+        // Filter out-of-bounds threads
+        if ((tx >= edge_size) && (ty >= edge_size) && (ty < blockDim.y - edge_size) && (tx < blockDim.x - edge_size)) {
+
+            // Find weak pixel and determine if it is adjacent to edge pixel
+            // Changed to add correct boundary
+            if (weak_chunk[ty][tx] == 1) { 
+                int sum = 0;
+                float weak_value = 1;
+                float edge_value = 0;
+
+                // Scan adjacent pixels
+                for (int edge_row = -edge_size; edge_row < edge_size+1; ++edge_row) {
+                    for (int edge_col = -edge_size; edge_col < edge_size+1; ++edge_col) {
+                        int curRow = ty + edge_row;
+                        int curCol = tx + edge_col;
+
+                        // Make sure adjacent pixels are not beyond the boundary of the image
+                        if ((curRow >= -1) && (curRow < height) && (curCol >= -1) && (curCol < width)) {
+
+						    // Sum all pixels in 3x3 neighborhood
+                            sum += edge_chunk[curRow][curCol];
+                        }
+                    }
+                }
+                // Subtract center pixel from sum
+			    sum = sum - edge_chunk[ty][tx];
+    
+                // Change weak edge to strong edge
+		        if (sum > 0) {
+				    weak_img[rel_row * width + rel_height]= 0;
+	                edge_img[rel_row * width + rel_height]= 1; 
+                }
+            }
+        }
+    }
+}
+
+*/
